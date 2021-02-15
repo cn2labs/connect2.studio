caad1a77134843077072aa094b7049760c511509
Jannik Baranczyk <jb@connect2.de>
2021-02-15 11:25:43 +0100

    add request block

--- src/components/blocks/request.js
+++ src/components/blocks/request.js
@@ -1,24 +1,50 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "gatsby"
+import Fade from "react-reveal/Fade"
 
 import Section from "../styles/section"
 import Grid from "../styles/grid"
 
+const RequestButton = styled.button`
+  --color: var(--lavender-soap);
+
+  background-color: transparent;
+  border: 4px solid var(--color);
+  color: var(--color);
+  cursor: pointer;
+  font-size: 2rem;
+  font-family: var(--headline-font);
+  line-height: 1.2;
+  padding: 20px 10px;
+  transition: background-color 0.1s ease-in, color 0.1s ease-in;
+  width: 100%;
+
+  &:hover {
+    background-color: var(--color);
+    color: #fff;
+  }
+`
+
 const SendARequest = () => (
-  <Section aside="Hey buddy">
-    <Grid repeatCols="3" gap="100">
-      <h4 className="size-h4">
-        Überzeugt? Nice! Wir freuen uns auf deine Anfrage.<sup>*</sup>
-      </h4>
-      <p>
-        <strong>
-          <sup>*</sup>Wenn du deine Rechnungen bezahlst, unsere Mails
-          beantwortest und nichts gegen Mate hast.
-        </strong>
-      </p>
-      <button>Jetzt anfragen</button>
-    </Grid>
-  </Section>
+  <Fade>
+    <Section>
+      <Grid repeatCols="3" gap="100" align="flex-start">
+        <h4 className="size-h4">
+          Überzeugt? Nice! Wir freuen uns auf deine Anfrage.<sup>*</sup>
+        </h4>
+        <p>
+          <strong>
+            <sup>*</sup>Du kannst uns anrufen, uns eine Mail schreiben, oder
+            unseren Anfrage-Konfigurator ausfüllen.
+          </strong>
+        </p>
+        <Link to="/anfrage">
+          <RequestButton>Jetzt anfragen</RequestButton>
+        </Link>
+      </Grid>
+    </Section>
+  </Fade>
 )
 
 export default SendARequest
--- src/components/styles/grid.js
+++ src/components/styles/grid.js
@@ -6,10 +6,11 @@ const GridStyles = styled.div`
   grid-template-columns: ${props =>
     props.repeatCols ? `repeat(${props.repeatCols}, 1fr)` : props.cols};
   grid-column-gap: ${props => props.gap}px;
+  ${props => props.align && `align-items: ${props.align};`}
 `
 
-const Grid = ({ children, repeatCols, cols, gap = "30" }) => (
-  <GridStyles cols={cols} repeatCols={repeatCols} gap={gap}>
+const Grid = ({ children, repeatCols, cols, gap = "30", align }) => (
+  <GridStyles cols={cols} repeatCols={repeatCols} gap={gap} align={align}>
     {children}
   </GridStyles>
 )
