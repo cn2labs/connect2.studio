b8971cd9b0b3f1651f786f263a522e5268f13eed
Kimberley George <kg@connect2.de>
2022-05-04 08:28:35 +0200

    Update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
+import Container from "../components/styles/container"
+
+const Spacer = styled.div`
+  padding: 11rem 0;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <Container>
+      <Spacer>
+        <h1>404: Not Found :(</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Spacer>
+    </Container>
   </Layout>
 )
 
